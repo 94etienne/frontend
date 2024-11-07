919eefbc77600b143b4ccbf0103bc54058270b52
NTAMBARA Etienne <65026243+94etienne@users.noreply.github.com>
2024-11-07 12:59:13 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-// import Bus from './pages/BusUI';
-import NotFound from './pages/NotFound';
+// // import Bus from './pages/BusUI';
+// import NotFound from './pages/NotFound';
 
-InvalidPage
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-{/*       <Route path="/bringback" element={<Navigate to="admin-dashboard" />}>
-          {" "}
-        </Route> */}
-{/*         <Route path="/" element={<Bus />} /> */}
-        <Route path="/" element={<NotFound />} />
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+// {/*       <Route path="/bringback" element={<Navigate to="admin-dashboard" />}>
+//           {" "}
+//         </Route> */}
+// {/*         <Route path="/" element={<Bus />} /> */}
+//         <Route path="/" element={<NotFound />} />
  
-      </Routes>
-    </BrowserRouter>
-  );
-}
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
-export default App;
+// export default App;
